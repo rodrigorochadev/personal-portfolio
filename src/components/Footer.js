8c2d2da6b7347d47e02edcf73da3dfe02598a237
Rodrigo Rocha <rodrigorochaua@gmail.com>
2020-09-27 03:18:17 +0100

    Case sensitive files fixed.

---
+++ src/components/Footer.js
@@ -0,0 +1,47 @@
+import React from 'react'
+import scrollTo from 'gatsby-plugin-smoothscroll'
+
+import { Container, FooterContainer, FooterSection, FooterCopyright, FooterSocial, FooterContent, FooterSVG, UnstyledButton } from '../styles/componentsStyles'
+import { Instagram, Dribbble, Behance, Logo, Linkedin, Github } from '../assets/svg/svg'
+
+export default () => {
+
+    return(
+        <FooterContainer id="footer">
+            <Container>
+                <FooterSVG>
+                    <FooterContent>
+                        <FooterSection>
+                            <UnstyledButton aria-label="rodrigorocha" onClick={() => scrollTo('#home')}>
+                                <Logo />
+                            </UnstyledButton>
+                        </FooterSection>
+                        <FooterSection>
+                            <FooterSocial>
+                                <a href="https://linkedin.com/in/rodrigorocha101" rel="noreferrer" target="_blank" aria-label="Linkedin">
+                                    <Linkedin />
+                                </a>
+                                <a href="https://github.com/rodrigo-rocha" rel="noreferrer" target="_blank" aria-label="Github">
+                                    <Github />
+                                </a>
+                                <a href="https://behance.net/rodrigo-rocha" rel="noreferrer" target="_blank" aria-label="Behance">
+                                    <Behance />
+                                </a>
+                                <a href="https://dribbble.com/rodrigorocha" rel="noreferrer" target="_blank" aria-label="Dribbble">
+                                    <Dribbble />
+                                </a>
+                                <a href="https://instagram.com/__rodrigorocha__" rel="noreferrer" target="_blank" aria-label="Instagram">
+                                    <Instagram />
+                                </a>
+                            </FooterSocial>
+                        </FooterSection>
+                        <FooterCopyright>
+                            <p>Rodrigo Rocha © {new Date().getFullYear()}. All rights reserved.</p>
+                        </FooterCopyright>
+                    </FooterContent>
+                </FooterSVG>
+            </Container>
+        </FooterContainer>
+    )
+
+}
---
+++ src/components/Seo.js
@@ -0,0 +1,108 @@
+/**
+ * SEO component that queries for data with
+ *  Gatsby's useStaticQuery React hook
+ *
+ * See: https://www.gatsbyjs.com/docs/use-static-query/
+ */
+
+import React from "react"
+import PropTypes from "prop-types"
+import { Helmet } from "react-helmet"
+import { useStaticQuery, graphql } from "gatsby"
+
+function SEO({ description, lang, meta, title }) {
+  const { site } = useStaticQuery(
+    graphql`
+      query {
+        site {
+          siteMetadata {
+            title
+            description
+            author
+            siteUrl
+            image
+            keywords
+          }
+        }
+      }
+    `
+  )
+
+  const metaDescription = description || site.siteMetadata.description
+  const defaultTitle = site.siteMetadata?.title
+
+  return (
+    <Helmet
+      htmlAttributes={{
+        lang,
+      }}
+      title={title}
+      titleTemplate={defaultTitle ? `%s | ${defaultTitle}` : null}
+      meta={[
+        {
+          name: `description`,
+          content: metaDescription,
+        },
+        {
+          name: `keywords`,
+          content: `${site.siteMetadata.keywords}`,
+        },
+        {
+          property: `og:title`,
+          content: `${title} - Rodrigo Rocha`,
+        },
+        {
+          property: `og:description`,
+          content: metaDescription,
+        },
+        {
+          property: `og:type`,
+          content: `website`,
+        },
+        {
+          property: `og:image`,
+          content: site.siteMetadata.image,
+        },
+        {
+          property: `og:image:alt`,
+          content: `Rodrigo Rocha`,
+        },
+        {
+          property: `og:url`,
+          content: site.siteMetadata.siteUrl,
+        },
+        {
+          name: `twitter:card`,
+          content: `summary_large_image`,
+        },
+        {
+          name: `twitter:creator`,
+          content: site.siteMetadata.author,
+        },
+        {
+          name: `twitter:title`,
+          content: title,
+        },
+        {
+          name: `twitter:description`,
+          content: metaDescription,
+        },
+      ].concat(meta)}
+    />
+  )
+}
+
+SEO.defaultProps = {
+  lang: `en`,
+  meta: [],
+  description: ``,
+}
+
+SEO.propTypes = {
+  description: PropTypes.string,
+  lang: PropTypes.string,
+  meta: PropTypes.arrayOf(PropTypes.object),
+  title: PropTypes.string.isRequired,
+}
+
+export default SEO
